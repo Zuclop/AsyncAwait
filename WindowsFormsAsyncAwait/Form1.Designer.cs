fcc3a7030935603887cae93874d1fb6f44cb57ce
Yevhen <Yevhen.boiko@gmail.com>
2016-11-07 21:17:00 +0200

    Init

---
+++ WindowsFormsAsyncAwait/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsAsyncAwait
+{
+    partial class FileLoggerForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddToLogButton = new System.Windows.Forms.Button();
+            this.FilePathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AddToLogButton
+            // 
+            this.AddToLogButton.Location = new System.Drawing.Point(12, 85);
+            this.AddToLogButton.Name = "AddToLogButton";
+            this.AddToLogButton.Size = new System.Drawing.Size(694, 23);
+            this.AddToLogButton.TabIndex = 0;
+            this.AddToLogButton.Text = "Add to Log";
+            this.AddToLogButton.UseVisualStyleBackColor = true;
+            this.AddToLogButton.Click += new System.EventHandler(this.AddToLogButton_Click);
+            // 
+            // FilePathTextBox
+            // 
+            this.FilePathTextBox.Location = new System.Drawing.Point(90, 59);
+            this.FilePathTextBox.Name = "FilePathTextBox";
+            this.FilePathTextBox.Size = new System.Drawing.Size(616, 20);
+            this.FilePathTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text File Path";
+            // 
+            // FileLoggerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 163);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.FilePathTextBox);
+            this.Controls.Add(this.AddToLogButton);
+            this.Name = "FileLoggerForm";
+            this.Text = "File Logger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddToLogButton;
+        private System.Windows.Forms.TextBox FilePathTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsAsyncAwait/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Concurrent;
+
+namespace WindowsFormsAsyncAwait
+{
+    public partial class FileLoggerForm : Form
+    {
+        private string logFileName = "File Log.txt";
+        private static object locker = new object();
+
+        public FileLoggerForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void AddToLogButton_Click(object sender, EventArgs e)
+        {
+            await LogFile(FilePathTextBox.Text);
+        }
+
+        private async Task LogFile(string sourcePath)
+        {
+            await Task.Run(() =>
+            {
+                lock (locker)
+                {
+                    try
+                    {
+                        byte[] buffer = new byte[1024];
+                        int readedBytes;
+                        using (FileStream DestinationStream = File.Open(logFileName, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read),
+                            SourceStream = File.Open(sourcePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                        {
+                            DestinationStream.Seek(0, SeekOrigin.End);
+                            while (true)
+                            {
+                                readedBytes = SourceStream.Read(buffer, 0, buffer.Length);
+                                if (readedBytes == 0)
+                                    break;
+                                DestinationStream.Write(buffer, 0, readedBytes);
+                            }
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            });
+        }
+    }
+}
